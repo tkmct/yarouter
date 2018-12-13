1f5cf2298f260596e8187f01170326576cf94aaf
tkmct <1220t.takamichi@gmail.com>
2018-12-14 00:00:55 +0900

    filter matched component

--- src/Route.js
+++ src/Route.js
@@ -1,3 +1,13 @@
+import { useContext } from 'react'
+import { RoutesContext } from './Router'
+
 export default function Route({ path, component }) {
-  return component()
+  const location = useContext(RoutesContext)
+
+  // TODO: extract to util function
+  if (path === location.pathname) {
+    return component()
+  }
+
+  return null
 }
--- src/Router.js
+++ src/Router.js
@@ -1,15 +1,16 @@
-import React from 'react'
+import React, { createContext } from 'react'
 import useLocation from './useLocation'
 import FloatingPanel from './utilComponents/FloatingPanel'
 import stringify from 'json-stringify-pretty-compact'
 
+export const RoutesContext = createContext(document.location || {})
+
 export default function Router({ children }) {
   const location = useLocation()
   return (
-    <>
-      <div>{children}</div>
-
+    <RoutesContext.Provider value={location}>
+      {children}
       <FloatingPanel>{stringify(location)}</FloatingPanel>
-    </>
+    </RoutesContext.Provider>
   )
 }
