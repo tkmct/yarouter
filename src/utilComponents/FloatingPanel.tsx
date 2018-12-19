a0cb1862b78f97b940f2fec23cabdf324f754396
tkmct <1220t.takamichi@gmail.com>
2018-12-19 15:09:48 +0900

    Fix floating panel

--- src/utilComponents/FloatingPanel.tsx
+++ src/utilComponents/FloatingPanel.tsx
@@ -14,6 +14,6 @@ const style: React.CSSProperties = {
   boxShadow: 'lightgrey 1px 1px 2px 1px',
 }
 
-export default function FloatingPanel({ children }: { children: React.ReactChildren }) {
+export default function FloatingPanel({ children }: { children: React.ReactNode }) {
   return <div style={style}>{children}</div>
 }
