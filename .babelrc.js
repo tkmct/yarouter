a16c8b4a7c9fa74e917e1dbd55ae827a0cf0728e
tkmct <1220t.takamichi@gmail.com>
2018-12-19 15:09:31 +0900

    Add @babel/plugin-transform-runtime

--- .babelrc.js
+++ .babelrc.js
@@ -1,7 +1,14 @@
 module.exports = {
-  presets: [
-    '@babel/preset-react',
-    '@babel/preset-typescript',
-    '@babel/preset-env',
+  presets: ['@babel/preset-react', '@babel/preset-typescript', '@babel/preset-env'],
+  plugins: [
+    [
+      '@babel/plugin-transform-runtime',
+      {
+        corejs: false,
+        helpers: true,
+        regenerator: true,
+        useESModules: false,
+      },
+    ],
   ],
 }
