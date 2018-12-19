57f51775a9bef3d6722b82f3c9680334f2ddf031
tkmct <1220t.takamichi@gmail.com>
2018-12-19 16:19:36 +0900

    Rename

--- src/hooks/useTransition.ts
+++ src/hooks/useTransition.ts
@@ -4,25 +4,25 @@ import { delay } from '../util'
 
 const useTransition = (history: History, transitionDuration: number) => {
   const [isTransitioning, setIsTransitioning] = useState(false)
-  const [location, setLocation] = useState(history.location) // location object
+  const [currentLocation, setCurrentLocation] = useState(history.location) // location object
   const [nextLocation, setNextLocation] = useState<Location | null>(null)
 
   // step1. set isTransitioning to true when location change
   useEffect(() => {
-    const unlisten = history.listen(async (nextLocation: Location, _: Action) => {
-      if (nextLocation.pathname !== location.pathname) {
-        setNextLocation(nextLocation)
+    const unlisten = history.listen(async (location: Location, _: Action) => {
+      if (location.pathname !== currentLocation.pathname) {
+        setNextLocation(location)
         setIsTransitioning(true)
         await delay(transitionDuration)
         setNextLocation(null)
-        setLocation(nextLocation)
+        setCurrentLocation(location)
         setIsTransitioning(false)
       }
     })
     return unlisten
   })
 
-  return { isTransitioning, location, nextLocation }
+  return { isTransitioning, currentLocation, nextLocation }
 }
 
 export default useTransition
