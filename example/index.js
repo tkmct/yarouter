17340346f1e0ec9eca6e11725606469656210700
tkmct <1220t.takamichi@gmail.com>
2018-12-20 14:14:17 +0900

    Add slide example

--- example/index.js
+++ example/index.js
@@ -16,7 +16,7 @@ function Hello({ transitionState }) {
   console.log('Hello is ', transitionState)
 
   return (
-    <div className={`container ${transitionState}`}>
+    <div className={`container fade ${transitionState}`}>
       <h1 style={{ color: 'grey', backgroundColor: 'pink', textAlign: 'center' }}>Hello</h1>
       <Link to="/world">To world</Link>
     </div>
@@ -26,10 +26,27 @@ function Hello({ transitionState }) {
 function World({ transitionState }) {
   console.log('World is ', transitionState)
   return (
-    <div className={`container ${transitionState}`}>
+    <div className={`container fade ${transitionState}`}>
       <h1 style={{ color: 'grey', backgroundColor: 'yellow', textAlign: 'center' }}>World</h1>
       <Link to="/">To Hello</Link>
-      <Link to="/world?q=hello">Query</Link>
+    </div>
+  )
+}
+
+function Slide1({ transitionState }) {
+  return (
+    <div className={`container slide ${transitionState}`}>
+      <h1 style={{ color: 'grey', backgroundColor: 'lightgreen', textAlign: 'center' }}>Slide1</h1>
+      <Link to="/slide2">To Slide2</Link>
+    </div>
+  )
+}
+
+function Slide2({ transitionState }) {
+  return (
+    <div className={`container slide ${transitionState}`}>
+      <h1 style={{ color: 'grey', backgroundColor: 'orange', textAlign: 'center' }}>Slide2</h1>
+      <Link to="/slide1">To Slide1</Link>
     </div>
   )
 }
@@ -42,6 +59,8 @@ function App() {
       <TransitionRouter history={history}>
         <Route exact path="/" component={Hello} onEnter={() => console.log('Enter hello!')} />
         <Route exact path="/world" component={World} onEnter={() => console.log('Enter world!')} />
+        <Route exact path="/slide1" component={Slide1} />
+        <Route exact path="/slide2" component={Slide2} />
         <Route path="/nested" component={Nested} />
       </TransitionRouter>
     </div>
