7a461a9e26518e2724f1c371a7bd94056c12f518
tkmct <1220t.takamichi@gmail.com>
2018-12-21 00:48:00 +0900

    Handle location change based on action

--- src/hooks/useTransition.ts
+++ src/hooks/useTransition.ts
@@ -11,31 +11,49 @@ const useTransition = (history: History, transitionDuration: number) => {
   const [currentTraisitionState, setCurrentTransitionState] = useState<TransitionState>('entered')
   const [nextTransitionState, setNextTransitionState] = useState<TransitionState | null>(null)
 
+  async function handlePush(location: Location) {
+    if (location.pathname !== currentLocation.pathname) {
+      setNextLocation(location)
+      setNextTransitionState('before-enter')
+      setCurrentTransitionState('before-leave')
+      setIsTransitioning(true)
+      await delay(10) // FixMe: better way
+      setNextTransitionState('enter')
+      setCurrentTransitionState('leave')
+      // TODO: handle click while isTransitioning is true
+      await delay(transitionDuration * 1.2)
+      setNextTransitionState('entered')
+      setCurrentTransitionState('left')
+      setIsTransitioning(false)
+      setNextLocation(null)
+      setCurrentLocation(location)
+      await delay(10) // FixMe: better way
+      setCurrentTransitionState('entered')
+      setNextTransitionState(null)
+    }
+  }
+
+  async function handlePop(location: Location) {
+    // TODO: implement
+  }
+
   // step1. set isTransitioning to true when location change
   useEffect(() => {
-    const unlisten = history.listen(async (location: Location, _: Action) => {
-      if (isTransitioning) {
-        // TODO: handle click while isTransitioning is true
+    const unlisten = history.listen(async (location: Location, action: Action) => {
+      // path is not changed. other property like search changed
+      if (location.pathname === currentLocation.pathname) {
+        return
       }
 
-      if (location.pathname !== currentLocation.pathname) {
-        setNextLocation(location)
-        setNextTransitionState('before-enter')
-        setCurrentTransitionState('before-leave')
-        setIsTransitioning(true)
-        await delay(10) // FixMe: better way
-        setNextTransitionState('enter')
-        setCurrentTransitionState('leave')
+      if (isTransitioning) {
         // TODO: handle click while isTransitioning is true
-        await delay(transitionDuration * 1.2)
-        setNextTransitionState('entered')
-        setCurrentTransitionState('left')
-        setIsTransitioning(false)
-        setNextLocation(null)
-        setCurrentLocation(location)
-        await delay(10) // FixMe: better way
-        setCurrentTransitionState('entered')
-        setNextTransitionState(null)
+        return
+      }
+
+      if (action === 'PUSH') {
+        handlePush(location)
+      } else if (action === 'POP') {
+        handlePop(location)
       }
     })
     return unlisten
